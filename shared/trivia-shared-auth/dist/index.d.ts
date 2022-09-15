8983391431e9e785f69f67ba3f4b4504ca68a018
Kyle Owsen <me@kylebyte.com>
2022-09-14 19:11:14 -0700

    Ignore shared dist

--- shared/trivia-shared-auth/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="qs" />
-/// <reference types="express" />
-export interface Credentials {
-    username: string;
-    password: string;
-}
-export declare const authMiddleware: import("express").RequestHandler<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs, Record<string, any>>;
