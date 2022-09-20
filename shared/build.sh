a1ae15e0e2652d774f55574cab61c850fae9cf75
Kyle Owsen <me@kylebyte.com>
2022-09-20 00:29:53 -0700

    Fixed dumbest bug ever

--- shared/build.sh
+++ shared/build.sh
@@ -3,6 +3,8 @@
 for d in */; do
     cd $d
     npm install --omit optional
+    npm run build:clean
     npm run build
     rm -rf node_modules && npm i --production --ignore-scripts
+    cd ..
 done
